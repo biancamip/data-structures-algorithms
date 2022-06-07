35212e7cd5c3b1356bac5974fe3601de0f2af578
Bianca Pelegrini <bmpelegrini@gmail.com>
2022-06-07 18:57:12 -0300

    remove dups from list

--- linked-lists/removeDups.ts
+++ linked-lists/removeDups.ts
@@ -2,8 +2,42 @@
 
 import { LinkedList } from "./classDefinition";
 
-function removeDups(list: LinkedList) {
-  return;
+function removeDups(list: LinkedList): LinkedList {
+  let hashMap = new Map<number, any>();
+
+  let currNode = rdList.head;
+  let prevNode = rdList.head;
+
+  while (currNode !== null) {
+    let onHash = hashMap.get(currNode.data);
+
+    if (onHash === undefined) {
+      hashMap.set(currNode.data, true);
+      prevNode = currNode;
+      currNode = currNode.next;
+    } else {
+      if (prevNode !== null) {
+        prevNode.next = currNode.next;
+        currNode = currNode.next;
+      }
+    }
+  }
+
+  return list;
 }
 
-console.log("hello world");
+let rdList = new LinkedList();
+rdList.insert(8);
+rdList.insert(2);
+rdList.insert(16);
+rdList.insert(32);
+rdList.insert(64);
+rdList.insert(32);
+rdList.insert(64);
+rdList.insert(2);
+rdList.insert(8);
+rdList.insert(4);
+rdList.insert(4);
+
+console.log("rdlist:\n", rdList.toString());
+console.log("dups removed:\n", removeDups(rdList).toString());
