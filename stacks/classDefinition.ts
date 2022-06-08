28c7e8f3f3d65f2d9022f51a6e11e59da8b8bf7d
Bianca Pelegrini <bmpelegrini@gmail.com>
2022-06-08 15:42:50 -0300

    stacks init

---
+++ stacks/classDefinition.ts
@@ -0,0 +1,69 @@
+// pg 96
+
+export class Stack {
+  // if head is null, list is empty
+  // head: ListNode | null;
+
+  constructor(data?: number) {
+    // if (!data) {
+    //   this.head = null;
+    // } else {
+    //   this.head = new ListNode(data);
+    // }
+  }
+
+  push(data: number): void {
+    // if (this.head === null) {
+    //   this.head = new ListNode(data);
+    //   return;
+    // }
+    // let curr = this.head;
+    // while (curr.next !== null) {
+    //   curr = curr.next;
+    // }
+    // curr.next = new ListNode(data);
+  }
+
+  pop(data: number): void {
+    // if (this.head === null) return;
+    // if (this.head.data === data) {
+    //   this.head = this.head.next;
+    //   return;
+    // }
+    // let curr = this.head;
+    // while (curr.next !== null) {
+    //   if (curr.next.data === data) {
+    //     curr.next = curr.next.next;
+    //     return;
+    //   }
+    //   curr = curr.next;
+    // }
+  }
+
+  isEmpty() {
+    // return this.head === null;
+  }
+
+  // generate string from stack in a friendlier way for better visualization
+  toString() {
+    // let nodesData: number[] = [];
+    // let currNode: ListNode | null = this.head;
+    // while (currNode !== null) {
+    //   nodesData.push(currNode.data);
+    //   currNode = currNode.next;
+    // }
+    // return nodesData.join(" -> ");
+  }
+}
+
+// let myList: LinkedList = new LinkedList(8);
+// myList.insert(16);
+// myList.insert(32);
+// myList.insert(64);
+// myList.insert(2);
+// myList.insert(4);
+
+// myList.remove(32);
+// myList.insert(32);
+
+// console.log("myList:", myList.toString());
