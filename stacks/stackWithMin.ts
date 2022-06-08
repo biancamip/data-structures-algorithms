29be37d89f787f9e18992bde0d1c79ec1b330a01
Bianca Pelegrini <bmpelegrini@gmail.com>
2022-06-08 16:31:12 -0300

    implement stack that keeps track of minimum value

--- stacks/stackWithMin.ts
+++ stacks/stackWithMin.ts
@@ -1,25 +1,20 @@
-// pg 96
+import { Stack, StackNode } from "./classDefinition";
 
-export class StackNode {
-  val: number;
-  next: StackNode | null = null;
-
-  constructor(val: number) {
-    this.val = val;
-  }
-}
-
-export class Stack {
+class StackMin {
   // if top is null, list is empty
   top: StackNode | null;
+  minVal: Stack;
 
   constructor(val?: number) {
     if (val !== undefined) {
       let node: StackNode = new StackNode(val);
       node.next = null;
+
       this.top = node;
+      this.minVal = new Stack(val);
     } else {
       this.top = null;
+      this.minVal = new Stack();
     }
   }
 
@@ -28,16 +23,32 @@ export class Stack {
     return this.top.val;
   }
 
+  min(): number {
+    return this.minVal.peek();
+  }
+
+  //remove this
+  printMin() {
+    return this.minVal.toString();
+  }
+
   push(val: number): void {
     let node = new StackNode(val);
     node.next = this.top;
     this.top = node;
+
+    let currMin = this.min();
+    if (val <= currMin) this.minVal.push(val);
   }
 
   pop(): number {
     if (this.top === null) throw "empty stack";
     let val = this.top.val;
     this.top = this.top.next;
+
+    let currMin = this.min();
+    if (currMin === val) this.minVal.pop();
+
     return val;
   }
 
@@ -58,13 +69,21 @@ export class Stack {
   }
 }
 
-let stk: Stack = new Stack(8);
-stk.push(16);
-stk.push(32);
-stk.push(32);
-stk.pop();
-stk.push(64);
-stk.push(2);
-stk.push(4);
+let stkMin: StackMin = new StackMin(8);
+stkMin.push(16);
+stkMin.push(32);
+stkMin.push(32);
+stkMin.pop();
+stkMin.push(64);
+stkMin.push(4);
+stkMin.push(-2);
+stkMin.push(4);
+console.log(`[stk] ${stkMin.toString()}\n [min] ${stkMin.min()} `);
 
-// console.log("stk:", stk.toString());
+stkMin.push(-1);
+stkMin.push(80);
+stkMin.push(-2);
+stkMin.pop();
+console.log(`[stk] ${stkMin.toString()}\n [min] ${stkMin.min()} `);
+
+console.log("print min", stkMin.printMin());
