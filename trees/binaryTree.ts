872896a2eea537c7906e3e2328f6c9113cf8ad9b
Bianca Pelegrini <bmpelegrini@gmail.com>
2022-06-09 17:18:18 -0300

    add insert on binary trees

--- trees/binaryTree.ts
+++ trees/binaryTree.ts
@@ -7,7 +7,21 @@ class BinTreeNode {
     this.value = name;
   }
 }
+
+function insert(root: BinTreeNode, val: number) {
+  if (root.left === null) {
+    root.left = new BinTreeNode(val);
+  } else if (root.right === null) {
+    root.right = new BinTreeNode(val);
+  } else {
+    // both l and r are not null, try to insert on left
+    insert(root.left, val);
+  }
+}
+
 function main(): void {
   let root = new BinTreeNode(8);
+  insert(root, 6);
+  insert(root, 4);
   return;
 }
