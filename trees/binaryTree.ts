421d2f9db5bdefca1bbacc231b1f6ba2fa4d6ac6
Bianca Pelegrini <bmpelegrini@gmail.com>
2022-06-09 17:18:07 -0300

    binary trees init

---
+++ trees/binaryTree.ts
@@ -0,0 +1,13 @@
+class BinTreeNode {
+  value: number;
+  left: BinTreeNode | null = null;
+  right: BinTreeNode | null = null;
+
+  constructor(name: number) {
+    this.value = name;
+  }
+}
+function main(): void {
+  let root = new BinTreeNode(8);
+  return;
+}
